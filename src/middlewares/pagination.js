b049286a7567fed2af37510dcf0ed46ca5b02863
ankitshahu <ankit@matchlog.delivery>
2022-07-21 11:50:51 +0530

    Bug fixed

--- src/middlewares/pagination.js
+++ src/middlewares/pagination.js
@@ -5,6 +5,9 @@
  * Description : Pagination
  */
 
+const common = require('@constants/common')
+const httpStatus = require('@generics/http-status')
+
 module.exports = (req, res, next) => {
 	req.pageNo = req.query.page && Number(req.query.page) > 0 ? Number(req.query.page) : 1
 
@@ -12,9 +15,16 @@ module.exports = (req, res, next) => {
 		req.query.limit && Number(req.query.limit) > 0 && Number(req.query.limit) <= 100 ? Number(req.query.limit) : 100
 
 	req.searchText = req.query.search && req.query.search != '' ? decodeURI(req.query.search) : ''
-
-	delete req.query.page
-	delete req.query.limit
-	next()
-	return
+	if (req.searchText.match(/^[A-Za-z0-9 ]+$/) || req.searchText == '') {
+		delete req.query.page
+		delete req.query.limit
+		next()
+		return
+	} else {
+		throw common.failureResponse({
+			message: 'Invalid seach text 😥',
+			statusCode: httpStatus.bad_request,
+			responseCode: 'CLIENT_ERROR',
+		})
+	}
 }
