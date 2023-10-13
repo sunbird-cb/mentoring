675740f29d4c82228ac88e3ed80eb0179b7b39d0
Nevil <nevil@tunerlabs.com>
2023-10-13 17:28:17 +0530

    updated list api to get details from extension

--- src/services/helper/user.js
+++ src/services/helper/user.js
@@ -4,6 +4,9 @@ const endpoints = require('@constants/endpoints')
 const common = require('@constants/common')
 const httpStatusCode = require('@generics/http-status')
 
+const menteeQueries = require('../../database/queries/userextension')
+const mentorQueries = require('../../database/queries/mentorextension')
+
 module.exports = class UserHelper {
 	/**
 	 * User list.
@@ -30,6 +33,30 @@ module.exports = class UserHelper {
 				'&search=' +
 				searchText
 			const userDetails = await request.get(apiUrl, false, true)
+
+			const ids = userDetails.data.result.data.map((item) => item.values[0].id)
+			let extensionDetails
+			if (userType == common.MENTEE_ROLE) {
+				extensionDetails = await menteeQueries.getUsersByUserIds(ids, {
+					attributes: ['user_id', 'rating'],
+				})
+			} else if (userType == common.MENTOR_ROLE) {
+				extensionDetails = await mentorQueries.getMentorsByUserIds(ids, {
+					attributes: ['user_id', 'rating'],
+				})
+			}
+
+			const extensionDataMap = new Map(extensionDetails.map((newItem) => [newItem.user_id, newItem]))
+
+			userDetails.data.result.data.forEach((existingItem) => {
+				const user_id = existingItem.values[0].id
+				if (extensionDataMap.has(user_id)) {
+					const newItem = extensionDataMap.get(user_id)
+					existingItem.values[0] = { ...existingItem.values[0], ...newItem }
+				}
+				delete existingItem.values[0].user_id
+			})
+
 			return common.successResponse({
 				statusCode: httpStatusCode.ok,
 				message: userDetails.data.message,
