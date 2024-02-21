e234159dc1afd614b3e5a4239c9ada0f2ee004f5
prajwal <prajwal@tunerlabs.com>
2024-02-21 09:52:05 +0530

    added 2 new columns

--- src/database/models/organizationExtension.js
+++ src/database/models/organizationExtension.js
@@ -33,6 +33,14 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING,
 				defaultValue: 'MENTOR_QS2',
 			},
+			mentee_visibility_policy: {
+				type: DataTypes.STRING,
+				defaultValue: 'CURRENT',
+			},
+			external_mentee_visibility_policy: {
+				type: DataTypes.STRING,
+				defaultValue: 'CURRENT',
+			},
 		},
 		{
 			sequelize,
