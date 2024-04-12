d7978910c67c2ddbec7a7bcbb11f5c2d30e98c3b
joffinjoy <joffinjoy@gmail.com>
2024-04-12 17:13:26 +0530

    Removed Extensions From Docker Up/Down Bat Files

--- src/scripts/setup/docker-compose-up.bat
+++ src/scripts/setup/docker-compose-up.bat
@@ -1,11 +1,11 @@
 @echo off
 
 rem Set environment variables
-set "users_env=%cd%\user_env.txt"
-set "interface_env=%cd%\interface_env.txt"
-set "scheduler_env=%cd%\scheduler_env.txt"
-set "notification_env=%cd%\notification_env.txt"
-set "mentoring_env=%cd%\mentoring_env.txt"
+set "users_env=%cd%\user_env"
+set "interface_env=%cd%\interface_env"
+set "scheduler_env=%cd%\scheduler_env"
+set "notification_env=%cd%\notification_env"
+set "mentoring_env=%cd%\mentoring_env"
 
 rem Run docker-compose
 docker-compose -f docker-compose-mentoring.yml up
