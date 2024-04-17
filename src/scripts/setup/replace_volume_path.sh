a9741bf4cdf360d6a468ea58801ef2dae8562273
joffinjoy <joffinjoy@gmail.com>
2024-04-17 16:26:56 +0530

    Minor MacOs Compatibility Fix

--- src/scripts/setup/replace_volume_path.sh
+++ src/scripts/setup/replace_volume_path.sh
@@ -5,8 +5,8 @@ DOCKER_COMPOSE_FILE="docker-compose-mentoring.yml"
 
 # Check if the Docker Compose file exists
 if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
-  echo "Error: Docker Compose file '$DOCKER_COMPOSE_FILE' does not exist."
-  exit 1
+    echo "Error: Docker Compose file '$DOCKER_COMPOSE_FILE' does not exist."
+    exit 1
 fi
 
 # Get the current directory path
@@ -17,7 +17,6 @@ ESCAPED_CURRENT_DIR=$(printf '%s\n' "$CURRENT_DIR" | sed -e 's/[\/&]/\\&/g')
 
 # Use sed to replace the path leading up to 'environment.ts' in the docker-compose file
 # The pattern ensures that it only replaces the path component up to ':/app/src/environments/environment.ts'
-sed -i "s|/[^:]*\(\/environment\.ts\):/app/src/environments/environment.ts|$ESCAPED_CURRENT_DIR\1:/app/src/environments/environment.ts|" "$DOCKER_COMPOSE_FILE"
+sed -i -e "s|/[^:]*\(\/environment\.ts\):/app/src/environments/environment.ts|$ESCAPED_CURRENT_DIR\1:/app/src/environments/environment.ts|" "$DOCKER_COMPOSE_FILE"
 
 echo "Updated volume path for 'environment.ts' in $DOCKER_COMPOSE_FILE"
-
