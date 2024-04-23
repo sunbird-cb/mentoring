58a68ff229d7b93d3ae7d0bce4973db7b98d0056
joffinjoy <joffinjoy@gmail.com>
2024-04-23 22:17:13 +0530

    Replicated Portal Env Files To Native Installation Directory

---
+++ documentation/2.5.6/native/envs/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	baseUrl: 'http://localhost:3569',
+	deepLinkUrl: 'https://mentored.shikshalokam.org',
+	dev: false,
+	name: 'debug environment',
+	privacyPolicyUrl: 'https://shikshalokam.org/mentoring/privacy-policy',
+	production: true,
+	sqliteDBName: 'mentoring.db',
+	staging: false,
+	termsOfServiceUrl: 'https://shikshalokam.org/mentoring/term-of-use',
+}
