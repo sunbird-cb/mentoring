e99796345051bacc877da85be6d20dea5ac213ef
joffinjoy <joffinjoy@gmail.com>
2024-04-28 02:02:38 +0530

    Added Output Directory For READMEs

--- documentation/readmeGenerator.js
+++ documentation/readmeGenerator.js
@@ -1,16 +1,24 @@
 const fs = require('fs')
 const readline = require('readline')
+const path = require('path')
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 })
 
+const outputDir = './readmes' // Define the output directory
+
 rl.question('Enter the version of the documentation (leave blank for latest): ', (input) => {
 	const version = input.trim() || 'latest' // Default to 'latest' if no input
 	const templatePath = './README-Template.md' // Path to your original markdown file
 	const dockerComposePath = `./${version}/dockerized/MentorEd-Setup-README.md`
 	const pm2Path = `./${version}/native/MentorEd-Setup-README.md`
+
+	// Ensure the output directory exists
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir)
+	}
 
 	fs.readFile(templatePath, 'utf8', (err, data) => {
 		if (err) {
@@ -41,12 +49,13 @@ rl.question('Enter the version of the documentation (leave blank for latest): ',
 				updatedMarkdown = updatedMarkdown.replace('{{PM2_DOCUMENTATION}}', pm2Content)
 
 				const outputFilename = version === 'latest' ? 'README.md' : `README-${version}.md`
+				const fullPath = path.join(outputDir, outputFilename) // Construct full path for the output file
 
-				fs.writeFile(outputFilename, updatedMarkdown, 'utf8', (err) => {
+				fs.writeFile(fullPath, updatedMarkdown, 'utf8', (err) => {
 					if (err) {
 						console.error(`Failed to write the updated README file: ${err.message}`)
 					} else {
-						console.log(`Updated README successfully written to ${outputFilename}`)
+						console.log(`Updated README successfully written to ${fullPath}`)
 					}
 					rl.close()
 				})
